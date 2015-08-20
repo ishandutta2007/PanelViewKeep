08adc720b6dd40f1770c7d0a6c2161cf49911ac8
Paul <peiche@gmail.com>
2015-08-20 17:34:12 -0500

    Retooled for new file paths

--- gruntfile.js
+++ gruntfile.js
@@ -1,144 +1,149 @@
 /*!
- * Panel View for Keep Gruntfile
- * http://eichefam.net/projects/keep
- * @author Paul Eiche
- */
- 
+* Panel View for Keep Gruntfile
+* http://eichefam.net/projects/keep
+* @author Paul Eiche
+*/
+
 /**
- * Grunt Module
- */
+* Grunt Module
+*/
 module.exports = function(grunt) {
-	
-    var target = grunt.option('target') || 'prod';
-    
-    grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-        copy: {
-            main: {
-                files: [
-                    {
-                        cwd: 'bower_components/jquery/dist',
-                        src: 'jquery.min.js',
-                        dest: 'js',
-                        expand: true
-                    }
-                ]
-            }
+
+  var target = grunt.option('target') || 'prod';
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    copy: {
+      main: {
+        files: [
+          {
+            cwd: 'bower_components/jquery/dist',
+            src: 'jquery.min.js',
+            dest: 'dist/js',
+            expand: true
+          },
+          {
+            cwd: 'assets/img',
+            src: '*',
+            dest: 'dist/img',
+            expand: true
+          }
+        ]
+      }
+    },
+    scsslint: {
+      allFiles: ['assets/sass/*.scss'],
+      options: {
+        config: '.scss-lint.yml',
+        reporterOutput: 'report/scss-lint-report.xml',
+        colorizeOutput: true
+      },
+    },
+    sass: {
+      dev: {
+        options: {
+          style: 'expanded',
+          noCache: true,
+          sourcemap: 'auto',
+          unixNewlines: true
         },
-        scsslint: {
-            allFiles: ['sass/*.scss'],
-            options: {
-                config: '.scss-lint.yml',
-				reporterOutput: 'report/scss-lint-report.xml',
-                colorizeOutput: true
-            },
-        },
-        sass: {
-            dev: {
-                options: {
-                    style: 'expanded',
-                    noCache: true,
-                    sourcemap: 'auto',
-                    unixNewlines: true
-                },
-                files: {
-                    'css/style.css': 'sass/style.scss'
-                }
-            },
-            prod: {
-                options: {
-                    style: 'compressed',
-                    noCache: true,
-                    sourcemap: 'none',
-                    unixNewlines: true
-                },
-                files: {
-                    'css/style.css': 'sass/style.scss'
-                }
-            },
-		},
-		autoprefixer: {
-            dist: {
-                files: {
-                    'css/style.css': 'css/style.css'
-                }
-            }
-        },
-        jshint: {
-            files: ['GruntFile.js', 'js/src/*.js'],
-            options: {
-                'globals': {
-                    jQuery: true,
-                    alert: true                }
-            }
-        },
-        uglify: {
-            build: {
-                files: [{
-                    expand: true,
-                    cwd: 'js/src',
-                    dest: 'js',
-                    src: '**/*.js',
-                    ext: '.min.js'
-                }]
-            }
-        },
-        watch: {
-			css: {
-				files: 'sass/*.scss',
-				tasks: ['scsslint', 'sass:' + target, 'autoprefixer']
-			},
-            javascript: {
-                files: 'js/src/*.js',
-                tasks: ['jshint', 'uglify']
-            }
-		},
-        compress: {
-            main: {
-                options: {
-                    mode: 'zip',
-                    archive: function() {
-                        return 'releases/keep.zip';
-                    }
-                },
-                files: [
-                    {
-                        expand: true,
-                        src: [
-                            '**',
-                            '!.*',
-                            '!*.md',
-                            '!bower.json',
-                            '!gruntfile.js',
-                            '!package.json',
-                            '!bower_components/**',
-                            '!js/src/**',
-                            '!node_modules/**',
-                            '!releases/**',
-                            '!report/**',
-                            '!sass/**'
-                        ]
-                    }
-                ]
-            }
+        files: {
+          'dist/css/style.css': 'assets/sass/style.scss'
         }
-	});
-    
-    grunt.loadNpmTasks('grunt-autoprefixer');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-compress');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-scss-lint');
-    
-    grunt.registerTask('build', ['copy', 'sass:' + target, 'autoprefixer', 'uglify']);
-	grunt.registerTask('validate', ['scsslint', 'jshint']);
-	grunt.registerTask('default', ['scsslint', 'watch']);
-    
-    grunt.registerTask('zip', 'Make a zip file for installation.', function() {
-        grunt.log.writeln('Zipping up the project.');
-        grunt.task.run('compress');
-    });
+      },
+      prod: {
+        options: {
+          style: 'compressed',
+          noCache: true,
+          sourcemap: 'none',
+          unixNewlines: true
+        },
+        files: {
+          'dist/css/style.css': 'assets/sass/style.scss'
+        }
+      },
+    },
+    autoprefixer: {
+      dist: {
+        files: {
+          'dist/css/style.css': 'dist/css/style.css'
+        }
+      }
+    },
+    jshint: {
+      files: ['GruntFile.js', 'assets/js/*.js'],
+      options: {
+        'globals': {
+          jQuery: true,
+          alert: true
+        }
+      }
+    },
+    uglify: {
+      build: {
+        files: [{
+          expand: true,
+          cwd: 'assets/js',
+          dest: 'dist/js',
+          src: '**/*.js'
+        }]
+      }
+    },
+    watch: {
+      css: {
+        files: 'assets/sass/*.scss',
+        tasks: ['scsslint', 'sass:' + target, 'autoprefixer']
+      },
+      javascript: {
+        files: 'assets/js/*.js',
+        tasks: ['jshint', 'uglify']
+      }
+    },
+    compress: {
+      main: {
+        options: {
+          mode: 'zip',
+          archive: function() {
+            return 'releases/keep.zip';
+          }
+        },
+        files: [
+          {
+            expand: true,
+            src: [
+              '**',
+              '!.*',
+              '!*.md',
+              '!bower.json',
+              '!gruntfile.js',
+              '!package.json',
+              '!bower_components/**',
+              '!dist/**',
+              '!node_modules/**',
+              '!releases/**',
+              '!report/**'
+            ]
+          }
+        ]
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-compress');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-scss-lint');
+
+  grunt.registerTask('build', ['copy', 'sass:' + target, 'autoprefixer', 'uglify']);
+  grunt.registerTask('validate', ['scsslint', 'jshint']);
+  grunt.registerTask('default', ['scsslint', 'watch']);
+
+  grunt.registerTask('zip', 'Make a zip file for installation.', function() {
+    grunt.log.writeln('Zipping up the project.');
+    grunt.task.run('compress');
+  });
 };
