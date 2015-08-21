ab73f6e6d9953db3796447d9e164c4b5ac3cf7dc
Paul <peiche@gmail.com>
2015-08-21 17:40:55 -0500

    Added events for attaching and detaching tabs

--- assets/js/events.js
+++ assets/js/events.js
@@ -7,13 +7,6 @@ chrome.windows.onRemoved.addListener(function(windowId) {
 	}
 });
 
-chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
-	if (tabId == KEEP_TAB_ID) {
-		KEEP_TAB_ID = 0;
-		KEEP_WINDOW_ID = 0;
-	}
-});
-
 chrome.windows.onCreated.addListener(function(window) {
 	chrome.windows.get(window.id, { populate: true }, function(window) {
 		if (window.tabs[0].url.indexOf(KEEP_URL) != -1) {
@@ -27,9 +20,47 @@ chrome.windows.onCreated.addListener(function(window) {
 	});
 });
 
+chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
+	if (tabId == KEEP_TAB_ID) {
+		KEEP_TAB_ID = 0;
+		KEEP_WINDOW_ID = 0;
+	}
+});
+
 chrome.tabs.onCreated.addListener(function(tab) {
 	if (tab.url.indexOf(KEEP_URL) != -1) {
 		KEEP_TAB_ID = tab.id;
 		KEEP_WINDOW_ID = tab.windowId;
+	}
+});
+
+chrome.tabs.onDetached.addListener(function(tabId, detachInfo) {
+	console.log('KEEP_TAB_ID: ' + KEEP_TAB_ID);
+	console.log('tabId: ' + tabId);
+	console.log(detachInfo);
+
+	if (tabId > 0) {
+		chrome.tabs.get(tabId, function(tab) {
+			if (tab.url.indexOf(KEEP_URL) != -1) {
+				KEEP_TAB_ID = tab.id;
+				KEEP_WINDOW_ID = tab.windowId;
+			}
+		});
+	}
+
+});
+
+chrome.tabs.onAttached.addListener(function(tabId, attachInfo) {
+	console.log('KEEP_TAB_ID: ' + KEEP_TAB_ID);
+	console.log('tabId: ' + tabId);
+	console.log(attachInfo);
+
+	if (tabId > 0) {
+		chrome.tabs.get(tabId, function(tab) {
+			if (tab.url.indexOf(KEEP_URL) != -1) {
+				KEEP_TAB_ID = tab.id;
+				KEEP_WINDOW_ID = tab.windowId;
+			}
+		});
 	}
 });
