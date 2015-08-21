714c2be7407e87d01989ed177992bd5ba925fcb4
Paul <peiche@gmail.com>
2015-08-21 17:41:10 -0500

    Restructured tasks

--- gruntfile.js
+++ gruntfile.js
@@ -9,24 +9,37 @@
 */
 module.exports = function(grunt) {
 
-  var target = grunt.option('target') || 'prod';
-
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    clean: {
+      build: [
+        'dist'
+      ]
+    },
     copy: {
       main: {
         files: [
           {
+            expand: true,
             cwd: 'bower_components/jquery/dist',
             src: 'jquery.min.js',
-            dest: 'dist/js',
-            expand: true
-          },
+            dest: 'dist/js'
+          }
+        ]
+      }
+    },
+    imagemin: {
+      png: {
+        options: {
+          optimizationLevel: 7
+        },
+        files: [
           {
+            expand: true,
             cwd: 'assets/img',
-            src: '*',
             dest: 'dist/img',
-            expand: true
+            src: '*.png',
+            ext: '.png'
           }
         ]
       }
@@ -80,23 +93,45 @@ module.exports = function(grunt) {
       }
     },
     uglify: {
-      build: {
+      dev: {
+        options: {
+          mangle: false,
+          beautify: true
+        },
         files: [{
           expand: true,
           cwd: 'assets/js',
           dest: 'dist/js',
-          src: '**/*.js'
+          src: '*.js',
+          ext: '.js'
+        }]
+      },
+      prod: {
+        options: {
+          compress: {
+            pure_funcs: [
+              'console.log',
+              'alert'
+            ]
+          }
+        },
+        files: [{
+          expand: true,
+          cwd: 'assets/js',
+          dest: 'dist/js',
+          src: '*.js',
+          ext: '.js'
         }]
       }
     },
     watch: {
       css: {
         files: 'assets/sass/*.scss',
-        tasks: ['scsslint', 'sass:' + target, 'autoprefixer']
+        tasks: ['scsslint', 'sass:dev', 'autoprefixer']
       },
       javascript: {
         files: 'assets/js/*.js',
-        tasks: ['jshint', 'uglify']
+        tasks: ['jshint', 'uglify:dev']
       }
     },
     compress: {
@@ -130,17 +165,22 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('grunt-autoprefixer');
-  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-compress');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-scss-lint');
 
-  grunt.registerTask('build', ['copy', 'sass:' + target, 'autoprefixer', 'uglify']);
   grunt.registerTask('validate', ['scsslint', 'jshint']);
-  grunt.registerTask('default', ['scsslint', 'watch']);
+
+  grunt.registerTask('dev', ['clean', 'copy', 'imagemin', 'sass:dev', 'autoprefixer', 'uglify:dev']);
+  grunt.registerTask('prod', ['clean', 'copy', 'imagemin', 'sass:prod', 'autoprefixer', 'uglify:prod']);
+
+  grunt.registerTask('default', ['scsslint', 'jshint',  'watch']);
 
   grunt.registerTask('zip', 'Make a zip file for installation.', function() {
     grunt.log.writeln('Zipping up the project.');
